ed8a5a3ebe95443286053973dd0a51fda4247db3
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-08-30 15:03:04 +0300

    Changes CredentialsConfig..

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -5,7 +5,9 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources({"classpath:config/credentials.properties"})
 public interface CredentialsConfig extends Config {
+    @Key ("login")
     String login ();
+    @Key ("password")
     String password ();
 }
 
